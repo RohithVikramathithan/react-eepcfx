0d28d30c96045ceac680327bde898160c6e9d068
RohithVikramathithan <rohith177043@gmail.com>
2021-02-01 11:48:32 +0530

    1 file modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,22 @@ import React from 'react';
 
 export default class Header extends React.Component{
 
+constructor(props)
+{
+  super();
+  this.state={name:'Well Hello newbie'}
+}
+update(e)
+{
+ this.setState({name:e.target.value});
+}
 render()
 {
   return(
-   <header>header</header>
+    <div>
+   <header>Hello ,{this.state.name}</header>
+   <input type='text' value={this.state.name} onChange={this.update.bind(this)} />
+   </div>
   );
 }
 }
