e3ecec9e040df6e63a4ef19c12b74c262e85baf2
RohithVikramathithan <rohith177043@gmail.com>
2021-02-03 12:17:36 +0530

    1 file modified

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,17 @@ export default class Header extends React.Component{
 constructor(props)
 {
   super();
-  this.state={name:'Well Hello newbie'}
+  this.state={name:'Well Hello newbie',count:0};
+  this.props={count:0};
 }
 update(e)
 {
  this.setState({name:e.target.value});
+}
+changeValue(e)
+{
+  var newCount=this.state.count+1;
+this.setState({count:newCount})
 }
 render()
 {
@@ -17,6 +23,8 @@ render()
     <div>
    <header>Hello ,{this.state.name}</header>
    <input type='text' value={this.state.name} onChange={this.update.bind(this)} />
+   <header>counter = {this.state.count}</header>
+   <button onClick={this.changeValue.bind(this)}>click for counter</button>
    </div>
   );
 }
